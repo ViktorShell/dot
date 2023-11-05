cb203c3dffdd7d6a1d6447095e4a5c702c89fa1f
viktor <viktor>
2023-11-05 21:41:02 +0100

    dot files

---
+++ config/fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  function fish_greeting
+    pfetch
+  end
+  
+  alias clipboard="xclip -selection clipboard"
+  alias ss "systemctl suspend -i"
+  alias sn "shutdown now"
+  alias r  "ranger" 
+  alias pd "pushd ."
+  alias pp "popd" 
+    
+  export VISUAL=nvim;
+  export EDITOR=nvim;
+  
+  # Root launch fish
+  function su
+   command su --shell=/usr/bin/fish $argv
+  end
+end
