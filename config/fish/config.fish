76321aa22f0d163ddb9083653c60ef9596aceb0d
viktor <viktor>
2024-01-03 10:29:02 +0100

    Aggiornato fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,10 +4,17 @@ if status is-interactive
     pfetch
   end
 
+  # Load tmux config
+  function tmux_source
+    tmux new -c "tmux source-file $HOME/.config/tmux/tmux.conf"
+  end
+
   alias clipboard="xclip -selection clipboard"
   alias ss "systemctl suspend -i"
-  alias sn "shutdown now"
+  alias sn "sync; shutdown now"
   alias r "ranger"
+  alias nv "nvim"
+  alias tm tmux_source 
     
   export VISUAL=nvim;
   export EDITOR=nvim;
