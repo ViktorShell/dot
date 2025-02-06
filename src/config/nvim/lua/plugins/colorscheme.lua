8a62352a5ebf4714170336734b938cfff9a660ec
Viktor <enricofiascov@gmail.com>
2025-02-06 14:31:51 +0100

    Added transparency to neovim ayu theme

--- src/config/nvim/lua/plugins/colorscheme.lua
+++ src/config/nvim/lua/plugins/colorscheme.lua
@@ -15,6 +15,19 @@ return {
 	priority = 1000,
 	opts = {},
 	config = function()
+		require("ayu").setup({
+			overrides = {
+				Normal = { bg = "None" },
+				NormalFloat = { bg = "none" },
+				ColorColumn = { bg = "None" },
+				SignColumn = { bg = "None" },
+				Folded = { bg = "None" },
+				FoldColumn = { bg = "None" },
+				CursorLine = { bg = "None" },
+				CursorColumn = { bg = "None" },
+				VertSplit = { bg = "None" },
+			},
+		})
 		vim.opt.background = "dark", vim.cmd("colorscheme ayu")
 	end,
 }
